e5748426071207a565b6031c53f849ca0d0b663e
Jay Joshi <joshi.jay.d@gmail.com>
2016-12-30 16:04:03 +0530

    If statement and loop in python

---
+++ ForLoopDemo.py
@@ -0,0 +1,11 @@
+foods = ["Parathe","Chole","Lassi","Dhokla","Biryani"]
+
+for f in foods:
+    print(f)
+    print(len(f))
+
+print("----------")
+
+for f in foods[:3]:
+    print(f)
+    print(len(f))
---
+++ IfElseDemo.py
@@ -0,0 +1,10 @@
+name = "Buccy"
+
+if name is "Jay":
+    print("Hi Jay")
+elif name is "Shra":
+    print("Hi baby")
+elif name is "Rohan":
+    print("Hey hero")
+else:
+    print("Hi dude")
