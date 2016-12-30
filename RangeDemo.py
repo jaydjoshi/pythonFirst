18186a1a2bd20fea2e295f3fbb071c10f6fb1904
Jay Joshi <joshi.jay.d@gmail.com>
2016-12-30 16:10:52 +0530

    Range demo

---
+++ RangeDemo.py
@@ -0,0 +1,17 @@
+for x in range(10,20):
+    print(x)
+
+print("----------")
+for x in range(10):
+    print(x)
+
+print("----------")
+for x in range(10,20,3):
+    print(x)
+
+print("----------")
+temp = 1
+
+while temp<10:
+    print(temp)
+    temp+=1
