a5475bb442bee3fc7caa3acd8754d91eb72742f5
Jay Joshi <joshi.jay.d@gmail.com>
2017-01-07 22:02:48 +0530

    Dictionary dmeo and name change

---
+++ Tut13-DictionaryDemo.py
@@ -0,0 +1,8 @@
+class_mates = {'Fenil': 'Awesome guy', 'Mani': 'Hard working', 'Shraddha': 'my love'}
+
+print(class_mates)
+
+print('----------------------------')
+
+for k, v in class_mates.items():
+    print(k + ' - ' + v)
